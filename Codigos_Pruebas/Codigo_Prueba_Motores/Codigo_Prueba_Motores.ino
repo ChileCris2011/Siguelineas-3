512c59658830883972b24801570999aa4842e01a
ChileCris2011 <112721430+ChileCris2011@users.noreply.github.com>
2025-05-24 10:31:19 -0400

    v0.31
    *Corrección de errores:
        Codigo de prueba para motores

--- Codigos_Pruebas/Codigo_Prueba_Motores/Codigo_Prueba_Motores.ino
+++ Codigos_Pruebas/Codigo_Prueba_Motores/Codigo_Prueba_Motores.ino
@@ -1,10 +1,13 @@
 #include "Pines.h"
 
+const int freq = 5000;
+const int resolution = 8;
+
 void setup() {
-  inicializarMotores
+  inicializarMotores();
   while (digitalRead(BOTON) == 0) {
   }
 }
 void loop() {
-  Motor(100, 100)
+  Motor(100, 100);
 }
