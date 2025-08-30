c8a6267b427ac4c216fcf6354ffb10c6122152d0
Ccris <primerciclo.crisbenja2011@gmail.com>
2025-08-30 12:29:39 -0400

    SeguidorBASE

---
+++ SeguidorBASE/Pines.h
@@ -0,0 +1,33 @@
+// Pines motores
+#define AIN1 16
+#define AIN2 17
+#define PWMA 4
+#define BIN1 5
+#define BIN2 18
+#define PWMB 19
+
+// Botón
+#define BOTON 12
+
+#define LED 2
+
+/***********************************
+Robot 2:
+  AIN1 16
+  AIN2 17
+  PWMA 4
+  BIN1 5
+  BIN2 18
+  PWMB 19
+
+  
+Robot 3:
+  AIN1 16
+  AIN2 17
+  PWMA 4
+  BIN1 5
+  BIN2 18
+  PWMB 19
+
+  Cable Verde Atrás
+************************************/
